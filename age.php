f6a27f3bd83f506ac1026ae7bb4098283f3082d3
bruno-bouvet <bouvet.bruno@gmail.com>
2016-09-19 14:27:07 +0200

    Initial commit

---
+++ age.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brunobouvet
+ * Date: 19/09/2016
+ * Time: 11:42
+ */
+
+echo "Hello World!";
+
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
+];
+
+$somme = 0;
+
+
+foreach ($students as $age )
+{
+    $age;
+}
+
+$moyenne= $students.count;
+echo $moyenne;
+
